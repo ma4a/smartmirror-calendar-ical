82391be0cf4a0f75e00517b94bad5a1f11a9bfbd
Matze <matze@local>
2016-06-22 00:09:43 +0200

    adapt to actual module template conventions

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 var express = require('express');
 var app = express();
+var fs = require('fs');
 
 var controller = require('./app/controller');
 
@@ -9,14 +10,30 @@ app.set('views', __dirname);
 app.set('view engine', 'jade');
 
 app.get('/', function (req, res) {
-  let url = 'https://calendar.google.com/calendar/ical/your-username%40gmail.com/private-abc123/basic.ics' //override for test purpose
-  let limit = 4;
-    controller.get({url: url, limit: limit})
-      .then((events) => {
-        res.render('app/view.jade', {
-          events: events
-        });
+
+  let pckg = fs.readFileSync('package.json');
+  pckg = JSON.parse(pckg);
+
+  let params = {};
+  params.data = true;
+  let widget = {};
+  widget._id = '1234567890';
+  widget.size = pckg.smartmirror.size[0];
+
+  params.url = 'https://calendar.google.com/calendar/ical/your-username%40gmail.com/private-abc123/basic.ics' //override for test purpose
+  params.limit = 4;
+
+  controller.get(params)
+    .then((events) => {
+      res.render('./index.jade', {
+        events: events,
+        widget: widget,
       });
+    })
+    .catch((err) => {
+      console.dir(err);
+      res.status(500).send(err);
+    });
 });
 
 app.listen(8080, function () {
